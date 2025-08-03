bf9992c402172f619b3f98833c7322419c3ed25c
Iam-samyog <msamyog37@gmail.com>
2025-08-03 17:31:54 +0545

    Replaced the Phone to insta contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -92,16 +92,24 @@ const Contact = () => {
       content: 'msamyog37@gmail.com',
       link: 'mailto:msamyog37@gmail.com'
     },
-    {
-      icon: (
-        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
-        </svg>
-      ),
-      title: 'Phone',
-      content: '+977 9765409928',
-      link: 'tel:+977 9765409928'
-    },
+  {
+  icon: (
+    <svg
+      className="w-6 h-6"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="20" height="20" x="2" y="2" rx="5" ry="5" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37z" />
+      <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  ),
+  title: 'Instagram',
+  content: '@iam.samyog',
+  link: 'https://instagram.com/iam.samyog'
+},
     {
       icon: (
         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
